63f18027f654eaec58158275e0a14c355558599e
Baskoro Indrayana <baskoroindrayana@gmail.com>
2024-05-20 14:46:08 +0000

    feat: add image indicator to card

--- src/components/organisms/Card/index.tsx
+++ src/components/organisms/Card/index.tsx
@@ -4,24 +4,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 const TEST_IMAGE_SRC: string = 'https://images-ssl.gotinder.com/u/fXC9j6Jy16wV99b6E2QHQn/965XggPbCUEE6H6HKQSjMG.webp?Policy=eyJTdGF0ZW1lbnQiOiBbeyJSZXNvdXJjZSI6IiovdS9mWEM5ajZKeTE2d1Y5OWI2RTJRSFFuLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE3MTY3MjM2OTV9fX1dfQ__&Signature=pnbWchSQUtzfj88VrdRro6khxTYFI5ZqgRKBfyDgojxBsGjs2dIznvFiAiYu7my6MXvxIaBlrHat-cf4Jgs6MZHV9l7nDWKzetnEITXvhdI0FJKqkhJXuKxK7qyF7GwamJLmPtZhHCJe8BEeKJbf42vpSuIdNjQNCDSwTlJ-or96PiistGWfNxaJPugFrXxxC-BcJl7lNtZza7OvRwnqXdEqXdsbtaR2sdrPfmDhpLaUQQyGEL90A8Rh2hivXAuSVFrRCNSfvGw47OM2IRliO77yb6zGDmmB3FJUwiJsG8AEpPa1X-ABNDGmNEC9cxzeZ0n1RWzJrCrWgOeE6cWKUw__&Key-Pair-Id=K368TLDEUPA6OI';
+const NUM_IMAGES: number = 4;
 
 class Card extends React.Component {
 	render(): React.ReactNode {
+		// TODO refactor into CardImageContainer molecule
+		let imageIndicator = [];
+		for (let i = 0; i < NUM_IMAGES; i++) {
+			imageIndicator.push(
+				<div className="indicator" style={{width: `${100/NUM_IMAGES}%`}}></div>
+			);
+		}
 		return (
-			<div className="card">
-				<img src={TEST_IMAGE_SRC} alt="Your recommended pair" />
-				<div className="card-info">
-					<div className="card-identifier">
-						<div className="card-name-age">
-							<span className="card-name">Kezia</span>
-							<span className="card-age">23</span>
+			<div className="container">
+				<div className="image-indicator">
+					{imageIndicator}
+				</div>
+				<img src={TEST_IMAGE_SRC} className="image" alt="Your recommended pair" />
+				<div className="info">
+					<div className="identifier">
+						<div className="name-age">
+							<span className="name">Kezia</span>
+							<span className="age">23</span>
 						</div>
-						<button type="button" className="card-reveal-button">
+						<button type="button" className="reveal-button">
 							<FontAwesomeIcon icon={faArrowUp} />
 						</button>
 					</div>
-					<div className="card-location">
-						<FontAwesomeIcon icon={faLocationDot} className="card-location-icon" />
+					<div className="location">
+						<FontAwesomeIcon icon={faLocationDot} className="location-icon" />
 						<span>22 km away</span>
 					</div>
 				</div>
