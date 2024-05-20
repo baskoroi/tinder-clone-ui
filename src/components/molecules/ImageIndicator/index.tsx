48fad0f5de7cff8e30cc8cc91768e90af4509a04
Baskoro Indrayana <baskoroindrayana@gmail.com>
2024-05-20 14:54:37 +0000

    refactor: take out ImageIndicator component

---
+++ src/components/molecules/ImageIndicator/index.tsx
@@ -0,0 +1,32 @@
+import React, { ReactNode } from 'react';
+import './style.css';
+
+interface ImageIndicatorProps {
+	numberOfImages: number;
+	currentIndex: number;
+}
+
+class ImageIndicator extends React.Component<ImageIndicatorProps> {
+	render(): ReactNode {
+		const { numberOfImages, currentIndex } = this.props;
+		let imageIndicator = [];
+		for (let i = 0; i < numberOfImages; i++) {
+			imageIndicator.push(
+				<div
+					className="indicator"
+					style={{
+						width: `${100/numberOfImages}%`,
+						backgroundColor: i === currentIndex ? '#ffffff' : '#dddddd',
+					}}
+				/>
+			);
+		}
+		return (
+			<div className="image-indicator">
+				{imageIndicator}
+			</div>
+		);
+	}
+}
+
+export default ImageIndicator;
--- src/components/organisms/Card/index.tsx
+++ src/components/organisms/Card/index.tsx
@@ -2,24 +2,15 @@ import React from 'react';
 import './style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import ImageIndicator from '../../molecules/ImageIndicator';
 
 const TEST_IMAGE_SRC: string = 'https://images-ssl.gotinder.com/u/fXC9j6Jy16wV99b6E2QHQn/965XggPbCUEE6H6HKQSjMG.webp?Policy=eyJTdGF0ZW1lbnQiOiBbeyJSZXNvdXJjZSI6IiovdS9mWEM5ajZKeTE2d1Y5OWI2RTJRSFFuLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE3MTY3MjM2OTV9fX1dfQ__&Signature=pnbWchSQUtzfj88VrdRro6khxTYFI5ZqgRKBfyDgojxBsGjs2dIznvFiAiYu7my6MXvxIaBlrHat-cf4Jgs6MZHV9l7nDWKzetnEITXvhdI0FJKqkhJXuKxK7qyF7GwamJLmPtZhHCJe8BEeKJbf42vpSuIdNjQNCDSwTlJ-or96PiistGWfNxaJPugFrXxxC-BcJl7lNtZza7OvRwnqXdEqXdsbtaR2sdrPfmDhpLaUQQyGEL90A8Rh2hivXAuSVFrRCNSfvGw47OM2IRliO77yb6zGDmmB3FJUwiJsG8AEpPa1X-ABNDGmNEC9cxzeZ0n1RWzJrCrWgOeE6cWKUw__&Key-Pair-Id=K368TLDEUPA6OI';
-const NUM_IMAGES: number = 4;
 
 class Card extends React.Component {
 	render(): React.ReactNode {
-		// TODO refactor into CardImageContainer molecule
-		let imageIndicator = [];
-		for (let i = 0; i < NUM_IMAGES; i++) {
-			imageIndicator.push(
-				<div className="indicator" style={{width: `${100/NUM_IMAGES}%`}}></div>
-			);
-		}
 		return (
 			<div className="container">
-				<div className="image-indicator">
-					{imageIndicator}
-				</div>
+				<ImageIndicator numberOfImages={4} currentIndex={0} />
 				<img src={TEST_IMAGE_SRC} className="image" alt="Your recommended pair" />
 				<div className="info">
 					<div className="identifier">
