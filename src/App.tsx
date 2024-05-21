fab51e707d16dd9a470b6db984f9f675d7043ce9
Baskoro Indrayana <baskoroindrayana@gmail.com>
2024-05-21 08:16:20 +0000

    refactor: use image url and number of images as props

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faBell } from '@fortawesome/free-solid-svg-icons';
 import Card from './components/organisms/Card';
 
+const TEST_IMAGE_SRC: string = 'https://images-ssl.gotinder.com/u/fXC9j6Jy16wV99b6E2QHQn/965XggPbCUEE6H6HKQSjMG.webp?Policy=eyJTdGF0ZW1lbnQiOiBbeyJSZXNvdXJjZSI6IiovdS9mWEM5ajZKeTE2d1Y5OWI2RTJRSFFuLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE3MTY3MjM2OTV9fX1dfQ__&Signature=pnbWchSQUtzfj88VrdRro6khxTYFI5ZqgRKBfyDgojxBsGjs2dIznvFiAiYu7my6MXvxIaBlrHat-cf4Jgs6MZHV9l7nDWKzetnEITXvhdI0FJKqkhJXuKxK7qyF7GwamJLmPtZhHCJe8BEeKJbf42vpSuIdNjQNCDSwTlJ-or96PiistGWfNxaJPugFrXxxC-BcJl7lNtZza7OvRwnqXdEqXdsbtaR2sdrPfmDhpLaUQQyGEL90A8Rh2hivXAuSVFrRCNSfvGw47OM2IRliO77yb6zGDmmB3FJUwiJsG8AEpPa1X-ABNDGmNEC9cxzeZ0n1RWzJrCrWgOeE6cWKUw__&Key-Pair-Id=K368TLDEUPA6OI';
+
 class App extends React.Component {
   render(): React.ReactNode {
     return (
@@ -20,7 +22,7 @@ class App extends React.Component {
             </a>
           </div>
         </nav> 
-        <Card />
+        <Card imageUrl={TEST_IMAGE_SRC} numberOfImages={5} />
       </div>
     );
   }
--- src/components/organisms/Card/index.tsx
+++ src/components/organisms/Card/index.tsx
@@ -4,32 +4,36 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import ImageIndicator from '../../molecules/ImageIndicator';
 
-const TEST_IMAGE_SRC: string = 'https://images-ssl.gotinder.com/u/fXC9j6Jy16wV99b6E2QHQn/965XggPbCUEE6H6HKQSjMG.webp?Policy=eyJTdGF0ZW1lbnQiOiBbeyJSZXNvdXJjZSI6IiovdS9mWEM5ajZKeTE2d1Y5OWI2RTJRSFFuLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE3MTY3MjM2OTV9fX1dfQ__&Signature=pnbWchSQUtzfj88VrdRro6khxTYFI5ZqgRKBfyDgojxBsGjs2dIznvFiAiYu7my6MXvxIaBlrHat-cf4Jgs6MZHV9l7nDWKzetnEITXvhdI0FJKqkhJXuKxK7qyF7GwamJLmPtZhHCJe8BEeKJbf42vpSuIdNjQNCDSwTlJ-or96PiistGWfNxaJPugFrXxxC-BcJl7lNtZza7OvRwnqXdEqXdsbtaR2sdrPfmDhpLaUQQyGEL90A8Rh2hivXAuSVFrRCNSfvGw47OM2IRliO77yb6zGDmmB3FJUwiJsG8AEpPa1X-ABNDGmNEC9cxzeZ0n1RWzJrCrWgOeE6cWKUw__&Key-Pair-Id=K368TLDEUPA6OI';
+interface CardProps {
+  imageUrl: string;
+  numberOfImages: number;
+}
 
-class Card extends React.Component {
-	render(): React.ReactNode {
-		return (
-			<div className="container">
-				<ImageIndicator numberOfImages={4} currentIndex={0} />
-				<img src={TEST_IMAGE_SRC} className="image" alt="Your recommended pair" />
-				<div className="info">
-					<div className="identifier">
-						<div className="name-age">
-							<span className="name">Kezia</span>
-							<span className="age">23</span>
-						</div>
-						<button type="button" className="reveal-button">
-							<FontAwesomeIcon icon={faArrowUp} />
-						</button>
-					</div>
-					<div className="location">
-						<FontAwesomeIcon icon={faLocationDot} className="location-icon" />
-						<span>22 km away</span>
-					</div>
-				</div>
-			</div>
-		);
-	}
+class Card extends React.Component<CardProps> {
+  render(): React.ReactNode {
+    const { imageUrl, numberOfImages } = this.props;
+    return (
+      <div className="container">
+        <ImageIndicator numberOfImages={numberOfImages} currentIndex={0} />
+        <img src={imageUrl} className="image" alt="Your recommended pair" />
+        <div className="info">
+          <div className="identifier">
+            <div className="name-age">
+              <span className="name">Kezia</span>
+              <span className="age">23</span>
+            </div>
+            <button type="button" className="reveal-button">
+              <FontAwesomeIcon icon={faArrowUp} />
+            </button>
+          </div>
+          <div className="location">
+            <FontAwesomeIcon icon={faLocationDot} className="location-icon" />
+            <span>22 km away</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Card;
